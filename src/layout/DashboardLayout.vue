36d40f0566df8a3604ae8537063fdd645bbec895
Filipe <filipe@upplify.com>
2019-05-29 15:24:12 -0300

    Update dinamyc itens on sidebar dashboard

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -3,30 +3,16 @@
         <side-bar :background-color="sidebarBackground" short-title="Argon" title="Argon">
             <template slot="links">
                 <sidebar-item
-                    :link="{
-            name: 'Dashboard',
-            icon: 'ni ni-tv-2 text-primary',
-            path: '/dashboard'
-          }"
-                />
-
-                <sidebar-item
-                    :link="{name: 'Icons', icon: 'ni ni-planet text-blue', path: '/icons'}"
+                    :link="{name: 'Inicio', icon: 'ni ni-tv-2 text-blue', path: '/icons'}"
                 />
                 <sidebar-item
-                    :link="{name: 'Maps', icon: 'ni ni-pin-3 text-orange', path: '/maps'}"
+                    :link="{name: 'Items', icon: 'ni ni-box-2 text-blue', path: '/maps'}"
                 />
                 <sidebar-item
-                    :link="{name: 'User Profile', icon: 'ni ni-single-02 text-yellow', path: '/profile'}"
+                    :link="{name: 'Sagas', icon: 'ni ni-archive-2 text-blue', path: '/profile'}"
                 />
                 <sidebar-item
-                    :link="{name: 'Tables', icon: 'ni ni-bullet-list-67 text-red', path: '/tables'}"
-                />
-                <sidebar-item
-                    :link="{name: 'Login', icon: 'ni ni-key-25 text-info', path: '/login'}"
-                />
-                <sidebar-item
-                    :link="{name: 'Register', icon: 'ni ni-circle-08 text-pink', path: '/register'}"
+                    :link="{name: 'Amigos', icon: 'ni ni-single-02 text-blue', path: '/profile'}"
                 />
             </template>
         </side-bar>
