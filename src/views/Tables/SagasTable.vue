bb861a18cfe6985fa0dfaebba3c7cd373933318b
Filipe <filipe@upplify.com>
2019-05-30 10:37:24 -0300

    fix sagas table title

--- src/views/Tables/SagasTable.vue
+++ src/views/Tables/SagasTable.vue
@@ -3,7 +3,7 @@
         <div class="card-header border-0">
             <div class="row align-items-center">
                 <div class="col">
-                    <h3 class="mb-0">Items</h3>
+                    <h3 class="mb-0">Sagas</h3>
                 </div>
                 <div class="col text-right">
                     <button class="btn btn-sm btn-secondary">Adicionar</button>
