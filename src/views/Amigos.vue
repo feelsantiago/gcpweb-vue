678a203cc239b84b27c34034f84b9fd4999bc6ae
Filipe <filipe@upplify.com>
2019-05-30 10:38:15 -0300

    add amigos view

---
+++ src/views/Amigos.vue
@@ -0,0 +1,16 @@
+<template>
+    <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
+        <amigos-table></amigos-table>
+    </base-header>
+</template>
+
+<script>
+import AmigosTable from "./Tables/AmigosTable";
+
+export default {
+    name: "amigos",
+    components: {
+        AmigosTable
+    }
+};
+</script>
