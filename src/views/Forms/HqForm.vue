4000ee86ed0e27f10bcc5a2ff2359a8ede23c583
Filipe <filipe@upplify.com>
2019-06-04 11:14:03 -0300

    add hq form

---
+++ src/views/Forms/HqForm.vue
@@ -0,0 +1,12 @@
+<template>
+    <item-form></item-form>
+</template>
+
+<script>
+import ItemForm from "./ItemForm";
+
+export default {
+    name: "hq-form",
+    components: [ItemForm]
+};
+</script>
