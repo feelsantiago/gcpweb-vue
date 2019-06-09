baba0898c0901249c757c68bc7dfbadb6376e964
Filipe Santiago <porto.santiago@hotmail.com>
2019-06-09 16:25:38 -0300

    nao devolvidos

--- src/views/Dashboard/NaoDevolvidosTable.vue
+++ src/views/Dashboard/NaoDevolvidosTable.vue
@@ -1,61 +1,78 @@
 <template>
-    <div class="card">
-        <div class="card-header border-0">
-            <div class="row align-items-center">
-                <div class="col">
-                    <h3 class="mb-0">Ainda não devolvidos</h3>
-                </div>
-            </div>
+  <div class="card">
+    <div class="card-header border-0">
+      <div class="row align-items-center">
+        <div class="col">
+          <h3 class="mb-0">Ainda não devolvidos</h3>
         </div>
-
-        <div class="table-responsive">
-            <base-table thead-classes="thead-light" :data="tableData">
-                <template slot="columns">
-                    <th>Titulo</th>
-                    <th>Tipo</th>
-                    <th>Devolução</th>
-                    <th>Amigo</th>
-                    <th>Devolver</th>
-                </template>
-
-                <template slot-scope="{row}">
-                    <th scope="row">{{row.titulo}}</th>
-                    <td>{{row.tipo}}</td>
-                    <td>{{row.devolucao}}</td>
-                    <td>{{ row.amigo }}</td>
-                    <td>
-                        <base-button type="success" size="sm">Devolver</base-button>
-                    </td>
-                </template>
-            </base-table>
-        </div>
+      </div>
     </div>
+
+    <div class="table-responsive">
+      <base-table thead-classes="thead-light" :data="items">
+        <template slot="columns">
+          <th>Titulo</th>
+          <th>Tipo</th>
+          <th>Devolução</th>
+          <th>Amigo</th>
+          <th>Devolver</th>
+        </template>
+
+        <template slot-scope="{row, index}">
+          <th scope="row">{{row.titulo}}</th>
+          <td>{{row.tipo}}</td>
+          <td>{{row.devolucao}}</td>
+          <td>{{ row.amigo }}</td>
+          <td>
+            <base-button type="success" size="sm" @click="handleDevolver(row.id, index)">Devolver</base-button>
+          </td>
+        </template>
+      </base-table>
+    </div>
+  </div>
 </template>
 <script>
+import EmprestimoService from "../../services/EmprestimoService";
+import LoaderService from "../../services/LoaderService";
+
 export default {
-    name: "nao-devolvidos-table",
-    data() {
+  name: "nao-devolvidos-table",
+  async mounted() {
+    try {
+      const result = await EmprestimoService.abertos();
+      const data = result.data.content.map(element => {
         return {
-            total: 0,
-            tableData: [
-                {
-                    titulo: "HQ 1",
-                    tipo: "HQ",
-                    devolucao: "25/02/2020",
-                    amigo: "Thiago",
-                    devolvido: false
-                },
-                {
-                    titulo: "HQ 2",
-                    tipo: "HQ",
-                    devolucao: "25/02/2020",
-                    amigo: "Thiago",
-                    devolvido: false
-                }
-            ]
+          titulo: element.item.titulo,
+          tipo: element.item.tipo,
+          devolucao: new Date(element.devolucao).toLocaleDateString(),
+          amigo: element.amigo.nome,
+          id: element.item.id
         };
-    },
-    methods: {}
+      });
+
+      this.items = data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  data() {
+    return {
+      items: []
+    };
+  },
+  methods: {
+    async handleDevolver(id, index) {
+      try {
+        LoaderService.loading();
+        await EmprestimoService.devolver(id);
+        this.items.splice(index, 1);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        LoaderService.clear();
+      }
+    }
+  }
 };
 </script>
 <style>
