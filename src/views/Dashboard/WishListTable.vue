8d47f4417f5afcf13b9bc3cb2ec9fad47087111e
Filipe Santiago <porto.santiago@hotmail.com>
2019-06-09 16:06:33 -0300

    wishlist table

--- src/views/Dashboard/WishListTable.vue
+++ src/views/Dashboard/WishListTable.vue
@@ -1,64 +1,72 @@
 <template>
-    <div class="card">
-        <div class="card-header border-0">
-            <div class="row align-items-center">
-                <div class="col">
-                    <h3 class="mb-0">WishList</h3>
-                </div>
-                <div class="col text-right">
-                    <h3>Total: ${{ total }}</h3>
-                </div>
-            </div>
+  <div class="card">
+    <div class="card-header border-0">
+      <div class="row align-items-center">
+        <div class="col">
+          <h3 class="mb-0">WishList</h3>
         </div>
-
-        <div class="table-responsive">
-            <base-table thead-classes="thead-light" :data="tableData">
-                <template slot="columns">
-                    <th>Titulo</th>
-                    <th>Tipo</th>
-                    <th>Disponiblidade</th>
-                    <th>Preço</th>
-                    <th>Comprar</th>
-                </template>
-
-                <template slot-scope="{row}">
-                    <th scope="row">{{row.titulo}}</th>
-                    <td>{{row.tipo}}</td>
-                    <td>{{row.disponibilidade}}</td>
-                    <td>{{ '$' + row.preco }}</td>
-                    <td>
-                        <a :href="row.url" target="_blank" class="btn btn-sm btn-primary">Comprar</a>
-                    </td>
-                </template>
-            </base-table>
+        <div class="col text-right">
+          <h3>Total: ${{ total }}</h3>
         </div>
+      </div>
     </div>
+
+    <div class="table-responsive">
+      <base-table thead-classes="thead-light" :data="items">
+        <template slot="columns">
+          <th>Titulo</th>
+          <th>Tipo</th>
+          <th>Disponiblidade</th>
+          <th>Preço</th>
+          <th>Comprar</th>
+        </template>
+
+        <template slot-scope="{row}">
+          <th scope="row">{{row.titulo}}</th>
+          <td>{{row.tipo}}</td>
+          <td>{{row.disponibilidade}}</td>
+          <td>{{ '$' + row.preco }}</td>
+          <td>
+            <a :href="row.url" target="_blank" class="btn btn-sm btn-primary">Comprar</a>
+          </td>
+        </template>
+      </base-table>
+    </div>
+  </div>
 </template>
 <script>
+import ItemService from "../../services/ItemService";
+import LoaderService from "../../services/LoaderService";
+
 export default {
-    name: "wish-list-table",
-    data() {
-        return {
-            total: 0,
-            tableData: [
-                {
-                    titulo: "HQ 1",
-                    tipo: "HQ",
-                    disponibilidade: "25/02/2020",
-                    preco: "500",
-                    url: "http://www.google.com"
-                },
-                {
-                    titulo: "HQ 2",
-                    tipo: "HQ",
-                    disponibilidade: "25/02/2020",
-                    preco: "500",
-                    url: "http://www.google.com"
-                }
-            ]
-        };
-    },
-    methods: {}
+  name: "wish-list-table",
+  async mounted() {
+    try {
+      LoaderService.loading();
+      const result = await ItemService.getWishList();
+      this.items = result.data.content;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      LoaderService.clear();
+    }
+  },
+  data() {
+    return {
+      items: []
+    };
+  },
+  methods: {},
+  computed: {
+    total() {
+      if (this.items.length === 0) return 0;
+
+      const result = this.items
+        .map(item => item.preco)
+        .reduce((previus, next) => previus + next);
+      return result;
+    }
+  }
 };
 </script>
 <style>
